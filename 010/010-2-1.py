cad45901c791cfa4af9b61458170ca3c8364d19b
mindseeker <wanggangabc@gmail.com>
2016-10-19 20:58:06 +0800

    010

---
+++ 010/010-2-1.py
@@ -0,0 +1,7 @@
+member = ['xiaojiayu', 88, 'heiye', 90, 'mitu', 85, 'yijing', 90, 'qiuwuxieyang', 88]
+
+num = 0
+max_num = len(member)
+while num < max_num:
+    print(member[num], member[num + 1])
+    num += 2
---
+++ 010/010-2-2.py
@@ -0,0 +1,5 @@
+member = ['xiaojiayu', 88, 'heiye', 90, 'mitu', 85, 'yijing', 90, 'qiuwuxieyang', 88]
+
+for i in range(0, len(member)):
+    if i % 2 == 0:
+        print(member[i], member[i+1])
